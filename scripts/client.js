b84eac7626338417582b44bc76345c6618d489f0
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-07-13 13:02:06 -0500

    create addEmployee click handler, console test successful

--- scripts/client.js
+++ scripts/client.js
@@ -4,4 +4,14 @@ $(readyNow);
 
 function readyNow() {
     console.log('ready now');
+    $('#add-employee').on('click', addEmployee);
+}
+
+/**
+ * Gather input values for an employee and appends the data to the staffing table
+ */
+
+function addEmployee() {
+    console.log('in addEmployee');
+    
 }
