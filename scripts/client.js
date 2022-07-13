a27973736dde207e4947959bc5537bbfa11e6374
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-07-13 13:19:47 -0500

    newEmployee function to create an employee object with the input values as object properties, create currentStaff empty array to hold employee objects

--- scripts/client.js
+++ scripts/client.js
@@ -7,11 +7,32 @@ function readyNow() {
     $('#add-employee').on('click', addEmployee);
 }
 
+let currentStaff = [];
+
 /**
  * Gather input values for an employee and appends the data to the staffing table
  */
 
 function addEmployee() {
     console.log('in addEmployee');
-    
+    let firstName = $('#first-name-submit').val();
+    let lastName = $('#last-name-submit').val();
+    let iD = $('#id-submit').val();
+    let jobTitle = $('#title-submit').val();
+    let salary = $('#salary-input').val();
+    console.log(firstName, lastName, iD, jobTitle, salary);
+    newEmployee(firstName, lastName, iD, jobTitle, salary);
+}
+
+function newEmployee(firstNameInput, lastNameInput, numberInput, titleInput, salaryInput){
+    console.log('in newEmployee');
+    const employee = {
+        firstName: firstNameInput,
+        lastName: lastNameInput,
+        iD: numberInput,
+        title: titleInput,
+        salary: salaryInput
+    };
+    console.log(employee);
+    currentStaff.push(employee);
 }
